9fbce12fc79b8ed2ba386345e692ca96a9e6d451
ahrke <david.duy.minh@gmail.com>
2019-08-25 22:07:59 +0000

    Create entry page, and general UI outline with listArea, mapArea, and artistArea. Add functions to render artist info.

--- public/scripts/spotify.js
+++ public/scripts/spotify.js
@@ -1,33 +1,99 @@
- import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_KEY } from '../../config';
 
-// const request = require('request-promise-native');
+const getArtistsInfo = async (artists, access_token) => {
+  let artistsCollection = [];
 
-// const KEY = process.env.SPOTIFY;
+  for (let artist of artists) {
+    let aObj = {};
 
-// const getGlassAnimalsPromise = () => {
-//   return new Promise((res, rej) => {
-//     request('https://api.spotify.com/v1/search?q=glass%20animals&type=artist')
-//   })
-// }
+    // Search for artist in spotify database
+    let artistInfo = await $.ajax({
+      url: "https://api.spotify.com/v1/search?q=" + artist + "&type=artist",
+      type: "GET",
+      headers: {
+        "Authorization": "Bearer " + access_token
+      }
+    })
 
-$.get( {
-  url: 'https://api.spotify.com/v1/search?q=glass%20animals&type=artist',
-  headers: {
-    client_id: SPOTIFY_CLIENT_ID,
-    client_ket: SPOTIFY_CLIENT_KEY,
-    "Content-Type": 'application/json',
-    "Accept": "application/json"
+    // Create an object to hold artist information
+    let a = artistInfo.artists.items[0];
+    aObj.name = a.name;
+    aObj.href = a.href;
+    aObj.id = a.id;
+    aObj.image640 = a.images[0].url;
+
+    let albumUrl = `https://api.spotify.com/v1/artists/${a.id}/albums?market=ES&limit=2`
+    let albumInfo = await $.ajax({
+      url: albumUrl,
+      type: "GET",
+      headers: {
+        "Authorization": "Bearer " + access_token
+      }
+    })
+
+    aObj.iframeUrl = `https://open.spotify.com/embed/album/${albumInfo.items[0].id}`
+
+    artistsCollection.push(aObj);
   }
-})
-  .done(res => {
-    console.log(res)
-    alert(res)
-  })
 
-// $(document).ready(function() {
-//   alert("we ready!")
-//   console.log("we're ready!")
+  return artistsCollection;
+}
+
+const artistsDisplay = (res) => {
+  let artistCarouselItems = '';
+  for (let artist of res) {
+    artistCarouselItems += `
+      <div class="carousel-item active">
+        <div class='artist_container'>
+          <img src="${artist.image640}">
+          <div class="artist_info">
+            <h1 class='artist_name'>${artist.name}</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo eius voluptates nulla recusandae eveniet quaerat ducimus? Mollitia sequi delectus eveniet, vel placeat autem dicta eligendi repudiandae labore laboriosam aut quod, nemo eaque quibusdam odit, rem ipsa. Obcaecati, ratione iure numquam quam aut nobis vel cum itaque reiciendis debitis dolore, inventore quod vero. Vel fugiat neque voluptates nobis officiis sapiente earum!</p>
+            <iframe src="${artist.iframeUrl}" width="300" height="200" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+          </div>
+        </div>
+      </div>
+    `
+  }
+
+  document.querySelector("#artists").innerHTML += `
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        ${artistCarouselItems}
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+   `
+}
+// $.get( {
+//   url: 'https://api.spotify.com/v1/search?q=glass%20animals&type=artist',
+//   headers: {
+//     client_id: SPOTIFY_CLIENT_ID,
+//     "Content-Type": 'application/json',
+//     "Accept": "application/json"
+//   }
 // })
+//   .done(res => {
+//     console.log(res)
+//     alert(res)
+//   })
 
-// KEY = ''BQApPUBOqcWZojXe39nTW0TLVBnFDFazge655ogxblBq3pUa8tVzm97qCZ0flvRiyB5aIrJnh-pUJTRfh8citB-AUWW-6-6IyhadO62oDnkDV-WDT-1X56mHKz_BsTFQVMF32kJwQr35a8gOSKFdfQP1qXAlAufC8KNB9FK4IyLJyw510CiTNfPwGf4i1KLlL1SzZJqpGzPqYuQ2zaNRiOJbOdO2GAsrWaGr_HT-0FFAhia8ITmSXGn_aLmjFWy6NEay9W2i5hODQ4crQR7Pd77OEFX8wA'
-// https://api.spotify.com/v1/search?q=tania%20bowra&type=artist" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQApPUBOqcWZojXe39nTW0TLVBnFDFazge655ogxblBq3pUa8tVzm97qCZ0flvRiyB5aIrJnh-pUJTRfh8citB-AUWW-6-6IyhadO62oDnkDV-WDT-1X56mHKz_BsTFQVMF32kJwQr35a8gOSKFdfQP1qXAlAufC8KNB9FK4IyLJyw510CiTNfPwGf4i1KLlL1SzZJqpGzPqYuQ2zaNRiOJbOdO2GAsrWaGr_HT-0FFAhia8ITmSXGn_aLmjFWy6NEay9W2i5hODQ4crQR7Pd77OEFX8wA"
+//       let a = resAlbums
+
+//       $("#artistArea").css({ 'display' : 'block', 'flex' : 1 });
+//       document.querySelector("#artists").innerHTML += `
+//         <div class='artist_container'>
+//           <img src="${artist.image640}">
+//           <div class="artist_info">
+//             <h1 class='artist_name'>${artist.name}</h1>
+//             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo eius voluptates nulla recusandae eveniet quaerat ducimus? Mollitia sequi delectus eveniet, vel placeat autem dicta eligendi repudiandae labore laboriosam aut quod, nemo eaque quibusdam odit, rem ipsa. Obcaecati, ratione iure numquam quam aut nobis vel cum itaque reiciendis debitis dolore, inventore quod vero. Vel fugiat neque voluptates nobis officiis sapiente earum!</p>
+//             <iframe src="https://open.spotify.com/embed/album/${a.items[0].id}" width="300" height="200" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+//           </div>
+//         </div>
+//         `
